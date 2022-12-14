b4cb0778ea1960edfd2d007e8c1ce4bd56e978de
klmhyeonwooo <gentlemonster77@naver.com>
2022-12-14 19:03:53 +0900

    Axios, Style-Compoents 설치

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,21 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const [message1, setMessage1] = useState("");
   const [message2, setMessage2] = useState("");
 
   useEffect(() => {
-    fetch("/hello")
+    // fetch("/hello")
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     // setMessage(message => [...message, data]);
+    //     setMessage1(data[0]);
+    //     setMessage2(data[1]);
+    //   });
+    axios.get('/hello')
       .then((res) => {
-        return res.json();
+        console.log(res.data);
+        setMessage1(res.data[0]);
+        setMessage2(res.data[1]);
       })
-      .then((data) => {
-        // setMessage(message => [...message, data]);
-        setMessage1(data[0]);
-        setMessage2(data[1]);
-      });
   }, []);
 
   const Test = () => {
