bb90a0f216301c68bc359c0efd178fe21a7b41c3
gkfkktrh153 <77851079+gkfkktrh153@users.noreply.github.com>
2022-12-14 21:18:58 +0900

    wpqkf

--- src/main/java/com/sy/MyTodo/controller/HelloController.java
+++ src/main/java/com/sy/MyTodo/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.sy.MyTodo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,7 +8,14 @@ import java.util.List;
 @RestController
 public class HelloController {
     @GetMapping("hello")
-    public List<String> Hello(){
+    public List<String> getHello(){
         return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+    }
+
+    @PostMapping("hello")
+    public List<String> postHello(@RequestParam("contents") String contents, @RequestParam("title") String title)
+    {
+        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+
     }
 }
