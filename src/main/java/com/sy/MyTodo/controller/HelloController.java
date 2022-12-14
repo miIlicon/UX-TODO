9b8f32dbf2734c0e256c3280e5b505ad5678a6ba
gkfkktrh153 <77851079+gkfkktrh153@users.noreply.github.com>
2022-12-14 18:26:19 +0900

    first commit

---
+++ src/main/java/com/sy/MyTodo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.sy.MyTodo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public List<String> Hello(){
+        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+    }
+}
